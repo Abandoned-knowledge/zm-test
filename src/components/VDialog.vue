cc0b9f74de3fad48757a436dcd2abeee94c92d6b
gleb_ <gleb.evdokimov.1@gmail.com>
2025-10-08 09:11:19 +0300

    remove .modal__content

--- src/components/VDialog.vue
+++ src/components/VDialog.vue
@@ -23,15 +23,13 @@ const emit = defineEmits<{
 
 <template>
   <dialog class="modal" ref="dialog">
-    <div class="modal__content">
-      <h1 class="text-gradient">
-        ТЫ МОЛОДЕЦ!
-        <br />
-        <br />
-        <span class="text-accent">ВОТ ТВОИ {{ props.prize }}</span>
-      </h1>
-      <VButton @click="isOpenModel = false">забрать</VButton>
-    </div>
+    <h1 class="text-gradient">
+      ТЫ МОЛОДЕЦ!
+      <br />
+      <br />
+      <span class="text-accent">ВОТ ТВОИ {{ props.prize }}</span>
+    </h1>
+    <VButton @click="isOpenModel = false">забрать</VButton>
   </dialog>
 </template>
 
@@ -39,7 +37,7 @@ const emit = defineEmits<{
 .modal {
   border: solid clamp(0.375rem, 0.282vw + 0.309rem, 0.563rem) transparent;
   border-radius: clamp(2.5rem, 1.878vw + 2.06rem, 3.75rem);
-  background-image: linear-gradient(transparent, transparent),
+  background-image: var(--gradient-dialog-background),
     var(--gradient-dialog-border);
   background-origin: border-box;
   background-clip: padding-box, border-box;
@@ -48,6 +46,14 @@ const emit = defineEmits<{
   transition: all 0.3s ease-in-out allow-discrete;
   opacity: 0;
   transform: translateY(10%);
+  padding: clamp(1.5rem, 1.502vw + 1.148rem, 2.5rem);
+  flex-direction: column;
+  gap: clamp(3rem, 1.127vw + 2.736rem, 3.75rem);
+
+  h1 {
+    text-align: center;
+    -webkit-text-stroke-width: 2px;
+  }
 
   &::backdrop {
     opacity: 1;
@@ -56,25 +62,13 @@ const emit = defineEmits<{
   }
 
   &:open {
+    display: flex;
     opacity: 1;
     transform: translateY(0);
   }
 
   &:open::backdrop {
     background-color: rgba(32, 10, 67, 0.7);
-  }
-}
-
-.modal__content {
-  background-image: var(--gradient-dialog-background);
-  padding: clamp(1.5rem, 1.502vw + 1.148rem, 2.5rem);
-  display: flex;
-  flex-direction: column;
-  gap: clamp(3rem, 1.127vw + 2.736rem, 3.75rem);
-
-  h1 {
-    text-align: center;
-    -webkit-text-stroke-width: 2px;
   }
 }
 
